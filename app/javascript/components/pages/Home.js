f18b012d5b820b2543e008c2ca8022173ffd37da
Van Dexter <vanguardstellar@gmail.com>
2022-05-18 13:52:57 -0700

    updated per comments

--- app/javascript/components/pages/Home.js
+++ app/javascript/components/pages/Home.js
@@ -3,12 +3,9 @@ import React, { Component } from 'react'
 export default class Home extends Component {
   render() {
     return (
-      
 
-      
       <div>
-      <h3></h3> 
-      <img src={"https://images1.apartments.com/i2/3GB8lSM9nch450ptwywl9n3JzuAEG7FKmV2DdJclkUI/117/the-rylan-vista-ca-building-photo.jpg"} />
+        <img src={"https://images1.apartments.com/i2/3GB8lSM9nch450ptwywl9n3JzuAEG7FKmV2DdJclkUI/117/the-rylan-vista-ca-building-photo.jpg"} />
       </div>
 
     )
