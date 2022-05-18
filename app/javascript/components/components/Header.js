7d64a3bfcc85fe9b0433bfe902a635ae5df82bdf
Van Dexter <vanguardstellar@gmail.com>
2022-05-18 11:10:14 -0700

    first commit

--- app/javascript/components/components/Header.js
+++ app/javascript/components/components/Header.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Nav, NavItem } from 'reactstrap'
+import { Helmet } from "react-helmet"
 
 class Header extends Component {
   render() {
@@ -31,11 +32,35 @@ class Header extends Component {
               <a href={new_user_route} className="nav-link">Sign Up</a>
             </NavItem>
           }
-          <NavLink> 
-                
-          </NavLink>
         </Nav>
+
+        <div>
+          <Helmet>
+            <style>{'body { background-color:#2cd5eb; }'}</style>
+          </Helmet>
+
+        </div>
+
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+          <a class="navbar-brand" href="/">Home</a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+              <li class="nav-item active">
+                <a class="nav-link" href="/apartmentindex">See All Apartments <span class="sr-only"></span></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="/apartmentnew">Upload an Apartment Listing</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+
       </>
+
     )
   }
 }
--- app/javascript/components/pages/Home.js
+++ app/javascript/components/pages/Home.js
@@ -7,7 +7,7 @@ export default class Home extends Component {
 
       
       <div>
-      <h3>Home</h3> 
+      <h3></h3> 
       <img src={"https://images1.apartments.com/i2/3GB8lSM9nch450ptwywl9n3JzuAEG7FKmV2DdJclkUI/117/the-rylan-vista-ca-building-photo.jpg"} />
       </div>
 
